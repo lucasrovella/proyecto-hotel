96edd9637f7b0f5faeb26fa950ec72324963082d
Lucas <lucasrovella323@gmail.com>
2025-03-14 11:12:12 -0300

    Proyecto hotel

---
+++ main.js
@@ -0,0 +1,80 @@
+let ubicacionPrincipal = window.pageYOffset;
+
+    if (window.innerWidth <= 800) {
+        document.getElementById('navbar').style.top = '0';
+    } 
+    else {
+        window.onscroll = () => {
+            let Desplazamiento_Actual = window.pageYOffset;
+            if (ubicacionPrincipal >= Desplazamiento_Actual) {
+                document.getElementById('navbar').style.top = '0';
+            } else {
+                document.getElementById('navbar').style.top = '-200px';
+            }
+            ubicacionPrincipal = Desplazamiento_Actual;
+        }
+    }
+
+
+
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      slider = document.querySelector("#slider"),
+      sliderSection = document.querySelectorAll(".slider-section");
+
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+// setInterval(() => {
+//     moveToRight()
+// }, 3000);
+
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRight() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    counter++;
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+}  
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0 ) {
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    operacion = operacion - widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+    
+}   
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
